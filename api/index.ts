1275802a72658f7a85cacb827f50e80fc4fa76e9
Kachi <opd.livmind@gmail.com>
2026-07-09 09:25:28 +0100

    fix: rewrite /api/* to index with __path query and handle in wrapper

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,12 @@
+import { parse } from 'url'
+
 export default async function handler(req: any, res: any) {
+  const parsed = parse(req.url || '', true)
+  if (parsed.query.__path) {
+    req.url = '/' + parsed.query.__path
+  } else if (req.url?.startsWith('/api/')) {
+    req.url = req.url.slice(4)
+  }
   const { default: app } = await import('./app.js')
   return app(req, res)
 }
