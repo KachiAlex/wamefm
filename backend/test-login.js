8614434d05caa70486f18ec82f9c3186f2f4f797
Kachi <opd.livmind@gmail.com>
2026-07-12 02:03:17 +0100

    chore: remove test file

--- backend/test-login.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const data = JSON.stringify({email:'test@test.com',password:'test123'});
-const req = http.request({hostname:'localhost',port:3000,path:'/api/auth/login',method:'POST',headers:{'Content-Type':'application/json','Content-Length':data.length}},res=>{
-  let body='';
-  res.on('data',c=>body+=c);
-  res.on('end',()=>console.log(res.statusCode,body));
-});
-req.write(data);
-req.end();
